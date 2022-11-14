862102972ab2cd0673083e648bdae9d6ad6e9fab
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2022-11-14 23:13:46 +0300

    Created "IProductRepository"

--- Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();                       //получаем список всех товаров
+        Task<ProductDto> GetProductById(int productId);                    //получаем ссылку на один из товаров
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);       //создаём или обновляем продукт
+        Task<bool> DeleteProduct(int productId);                           //удаляем продукт
     }
 }
